915c31ae937c288489a33b37d4cf5949c2740303
Migue765 <ma.gomeza1@uniandes.edu.co>
2026-03-02 22:11:18 -0500

    update host database

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "34.224.212.111",  # Dirección IP de la base de datos
+        "HOST": "52.90.148.8",  # Dirección IP de la base de datos
         "PORT": "5432",  # Puerto de la base de datos
     }
 }
